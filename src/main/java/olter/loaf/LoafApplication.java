cea09f9298f944d4a19c8825a2955c701b34f920
olteR <olterver@gmail.com>
2023-06-06 17:56:38 +0200

    initial commit

---
+++ src/main/java/olter/loaf/LoafApplication.java
@@ -0,0 +1,13 @@
+package olter.loaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/olter/loaf/LoafApplicationTests.java
@@ -0,0 +1,13 @@
+package olter.loaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
